876311fbc106f370ec8d3a540c9fcd7ae0a6bfc4
Jacer OMRI <jaceromri@gmail.com>
2017-02-09 18:07:55 +0100

    refactor renderer
    update renderer based on latest version of oro email renderer

--- Provider/PDFTemplateRenderer.php
+++ Provider/PDFTemplateRenderer.php
@@ -2,6 +2,7 @@
 
 namespace Ibnab\Bundle\PmanagerBundle\Provider;
 
+use Oro\Bundle\EmailBundle\Provider\EmailRenderer;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 
@@ -14,7 +15,12 @@ use Ibnab\Bundle\PmanagerBundle\Model\PDFTemplateInterface;
 
 class PDFTemplateRenderer extends \Twig_Environment
 {
-    const VARIABLE_NOT_FOUND = 'ibnab.pmanager.variable.not.found';
+    const VARIABLE_NOT_FOUND = 'oro.email.variable.not.found';
+
+    /**
+     * @var array Map of filters to apply to template variables by default
+     */
+    public static $defaultVariableFilters = ['system.userSignature' => 'oro_html_sanitize'];
 
     /** @var VariablesProvider */
     protected $variablesProvider;
@@ -30,6 +36,9 @@ class PDFTemplateRenderer extends \Twig_Environment
 
     /** @var PropertyAccessor */
     protected $accessor;
+
+    /** @var array */
+    private $systemVariables;
 
     /**
      * @param \Twig_LoaderInterface   $loader
@@ -55,8 +64,8 @@ class PDFTemplateRenderer extends \Twig_Environment
         $this->sandBoxConfigCache = $cache;
         $this->cacheKey           = $cacheKey;
 
-        $this->addExtension($sandbox);
-        $this->configureSandbox();
+        /*$this->addExtension($sandbox);
+        $this->configureSandbox();*/
 
         $this->translator = $translator;
     }
@@ -67,8 +76,21 @@ class PDFTemplateRenderer extends \Twig_Environment
      */
     protected function configureSandbox()
     {
+        $allowedData = $this->getConfiguration();
+        /** @var \Twig_Extension_Sandbox $sandbox */
+        $sandbox = $this->getExtension('sandbox');
+        /** @var \Twig_Sandbox_SecurityPolicy $security */
+        $security = $sandbox->getSecurityPolicy();
+        $security->setAllowedProperties($allowedData['properties']);
+        $security->setAllowedMethods($allowedData['methods']);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getConfiguration()
+    {
         $allowedData = $this->sandBoxConfigCache->fetch($this->cacheKey);
-        
         if (false === $allowedData) {
             $allowedData = $this->prepareConfiguration();
             $this->sandBoxConfigCache->save($this->cacheKey, serialize($allowedData));
@@ -76,15 +98,7 @@ class PDFTemplateRenderer extends \Twig_Environment
             $allowedData = unserialize($allowedData);
         }
 
-        /** @var \Twig_Extension_Sandbox $sandbox */
-        $sandbox = $this->getExtension('sandbox');
-        /** @var \Twig_Sandbox_SecurityPolicy $security */
-        $security = $sandbox->getSecurityPolicy();
-        if(isset($allowedData['properties']) && isset($allowedData['methods']))
-        {
-        $security->setAllowedProperties($allowedData['properties']);
-        $security->setAllowedMethods($allowedData['methods']);
-        }
+        return $allowedData;
     }
 
     /**
@@ -94,25 +108,68 @@ class PDFTemplateRenderer extends \Twig_Environment
      */
     private function prepareConfiguration()
     {
-        $configuration = array();
-
-        $allGetters = $this->variablesProvider->getEntityVariableGetters();
+        $configuration               = [];
+        $configuration['formatters'] = [];
+        $allGetters                  = $this->variablesProvider->getEntityVariableGetters();
         foreach ($allGetters as $className => $getters) {
-            $properties = [];
-            $methods    = [];
+            $properties        = [];
+            $methods           = [];
+            $formatters        = [];
+            $defaultFormatters = [];
             foreach ($getters as $varName => $getter) {
                 if (empty($getter)) {
                     $properties[] = $varName;
                 } else {
-                    $methods[] = $getter;
+                    if (!is_array($getter)) {
+                        $methods[] = $getter;
+                    } else {
+                        $methods[]                   = $getter['property_path'];
+                        $formatters[$varName]        = $getter['formatters'];
+                        $defaultFormatters[$varName] = $getter['default_formatter'];
+                    }
                 }
             }
 
             $configuration['properties'][$className] = $properties;
             $configuration['methods'][$className]    = $methods;
+
+            $configuration['formatters'][$className]        = $formatters;
+            $configuration['default_formatter'][$className] = $defaultFormatters;
         }
 
         return $configuration;
+    }
+
+    /**
+     * Renders content with default filters
+     *
+     * @param string $content
+     * @param array  $templateParams
+     *
+     * @return string
+     */
+    public function renderWithDefaultFilters($content, array $templateParams = [])
+    {
+        $templateParams['system'] = $this->getSystemVariableValues();
+        $content = $this->addDefaultVariableFilters($content);
+
+        if (array_key_exists('entity', $templateParams)) {
+            $content = $this->processDefaultFilters($content, $templateParams['entity']);
+        }
+
+        return $this->render($content, $templateParams);
+    }
+
+    /**
+     * Compile preview content
+     *
+     * @param EmailTemplate $entity
+     *
+     * @return string
+     */
+    public function compilePreview(PDFTemplate $entity)
+    {
+        return $this->render('{% verbatim %}' . $entity->getContent() . '{% endverbatim %}', []);
     }
 
     /**
@@ -139,35 +196,6 @@ class PDFTemplateRenderer extends \Twig_Environment
         return array(null, $templateRendered);
     }
 
-    /**
-     * Compile preview content
-     *
-     * @param EmailTemplate $entity
-     *
-     * @return string
-     */
-    public function compilePreview(PDFTemplate $entity)
-    {
-        return $this->render('{% verbatim %}' . $entity->getContent() . '{% endverbatim %}', []);
-    }
-
-    /**
-     * Process entity variables of dateTime type
-     *   -- datetime variables with formatting will be skipped, e.g. {{ entity.createdAt|date('F j, Y, g:i A') }}
-     *   -- processes ONLY variables that passed without formatting, e.g. {{ entity.createdAt }}
-     *
-     * Note:
-     *  - add oro_format_datetime filter to all items which implement \DateTimeInterface
-     *  - if value does not exists and PropertyAccess::getValue throw an error
-     *    it will change on self::VARIABLE_NOT_FOUND
-     *  - all tags that do not start with `entity` will be ignored
-     *
-     * TODO find a common way for processing formatter
-     * @param string $template
-     * @param object $entity
-     *
-     * @return EmailTemplate
-     */
     protected function processDateTimeVariables($template, $entity)
     {
         $that     = $this;
@@ -175,16 +203,88 @@ class PDFTemplateRenderer extends \Twig_Environment
             $result = $match[0];
             $path   = $match[1];
             $split  = explode('.', $path);
-
             if ($split[0] && 'entity' === $split[0]) {
                 unset($split[0]);
-
                 try {
                     $value = $that->getValue($entity, implode('.', $split));
-
                     if ($value instanceof \DateTime || $value instanceof \DateTimeInterface) {
                         $result = sprintf('{{ %s|oro_format_datetime }}', $path);
                     }
+                } catch (\Exception $e) {
+                    $result = $that->translator->trans(self::VARIABLE_NOT_FOUND);
+                }
+            }
+            return $result;
+        };
+        return preg_replace_callback('/{{\s([\w\d\.\_\-]*?)\s}}/u', $callback, $template);
+    }
+
+
+    /**
+     * Parses $template and appends filters to the variables defined in self::$defaultVariableFilters map
+     *
+     * @param string $template
+     *
+     * @return string
+     */
+    protected function addDefaultVariableFilters($template)
+    {
+        foreach (self::$defaultVariableFilters as $var => $filter) {
+            $template = preg_replace('/{{\s' . $var . '\s}}/u', sprintf('{{ %s|%s }}', $var, $filter), $template);
+        }
+
+        return $template;
+    }
+
+    /**
+     * Process entity variables what have default filters, for example, datetime form type field
+     *
+     * Note:
+     *  - all tags that do not start with `entity` will be ignored
+     *
+     * @param string $template
+     * @param object $entity
+     *
+     * @return EmailTemplate
+     */
+    protected function processDefaultFilters($template, $entity)
+    {
+        $that     = $this;
+        $config   = $that->getConfiguration();
+        $callback = function ($match) use ($entity, $that, $config) {
+            $result = $match[0];
+            $path   = $match[1];
+            $split  = explode('.', $path);
+
+            if ($split[0] && 'entity' === $split[0]) {
+                unset($split[0]);
+                try {
+                    $propertyPath = array_pop($split);
+                    $value        = $entity;
+                    if (count($split)) {
+                        $value = $that->getValue($entity, implode('.', $split));
+                    }
+
+                    // check if value exists
+                    $that->getValue($value, $propertyPath);
+
+                    $propertyName = lcfirst(Inflector::classify($propertyPath));
+                    if (is_object($value) && array_key_exists('default_formatter', $config)) {
+                        $valueClass       = ClassUtils::getRealClass($value);
+                        $defaultFormatter = $config['default_formatter'];
+                        if (array_key_exists($valueClass, $defaultFormatter)
+                            && array_key_exists($propertyName, $defaultFormatter[$valueClass])
+                            && !is_null($defaultFormatter[$valueClass][$propertyName])
+                        ) {
+                            return sprintf(
+                                '{{ %s|oro_format(\'%s\') }}',
+                                $path,
+                                $config['default_formatter'][ClassUtils::getRealClass($value)][$propertyName]
+                            );
+                        }
+                    }
+
+                    return sprintf('{{ %s|oro_html_sanitize }}', $path);
                 } catch (\Exception $e) {
                     $result = $that->translator->trans(self::VARIABLE_NOT_FOUND);
                 }
@@ -219,5 +319,17 @@ class PDFTemplateRenderer extends \Twig_Environment
         }
 
         return $this->accessor;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSystemVariableValues()
+    {
+        if (null === $this->systemVariables) {
+            $this->systemVariables = $this->variablesProvider->getSystemVariableValues();
+        }
+
+        return $this->systemVariables;
     }
 }
